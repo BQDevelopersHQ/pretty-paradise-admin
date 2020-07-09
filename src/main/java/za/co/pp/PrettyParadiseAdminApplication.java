e876faa21f367c25b5d657fc53c1e33f8ad7bc11
Quintin Kruger <36225919+QuintinKruger@users.noreply.github.com>
2020-07-09 18:48:41 +0200

    Openapi specification updated

---
+++ src/main/java/za/co/pp/PrettyParadiseAdminApplication.java
@@ -0,0 +1,12 @@
+package za.co.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyParadiseAdminApplication {
+    public static void main(String... args){
+        SpringApplication.run(PrettyParadiseAdminApplication.class, args);
+    }
+
+}
